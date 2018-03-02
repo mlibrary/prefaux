95a57fb2cc3cc3ecd3883b45e212288e300c8646
Bryan Hockey <bhock@umich.edu>
2018-03-02 14:56:21 -0600

    fix loading for protect

--- lib/prefaux/config.rb
+++ lib/prefaux/config.rb
@@ -1,10 +1,16 @@
+require "pathname"
+require "ettin"
+
 module Prefaux
   class << self
     attr_reader :settings
     attr_writer :env
 
     def root
-      @root ||= Pathname.new(__FILE__).dirname.parent
+      @root ||= Pathname.new(__FILE__)
+        .dirname
+        .parent
+        .parent
     end
 
     def env
@@ -18,3 +24,5 @@ module Prefaux
 
   end
 end
+
+Prefaux.root
